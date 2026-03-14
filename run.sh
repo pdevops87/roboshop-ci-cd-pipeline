49fd9f93f57d08e58dbc828881fb0eff7df9a87b
Your Name <vemasani.pavani@gmail.com>
2026-03-14 18:03:37 +0530

    github action runner

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 git pull
 docker build -t pavanidevops78/kubernetes:runner .
-./run.sh
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock pavanidevops78/kubernetes:runner
+#./run.sh
+#docker run -it -v /var/run/docker.sock:/var/run/docker.sock pavanidevops78/kubernetes:runner
